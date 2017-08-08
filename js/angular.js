f4595ec21110bd2c5307e502f30f491472ac9f85
Ira Ritchie Meek <iraritchiemeek@gmail.com>
2017-08-08 13:02:28 +0800

    finish algorith for real this time

--- js/angular.js
+++ js/angular.js
@@ -5,7 +5,7 @@ carLoansApp.controller('mainController', ["$scope", function($scope) {
 
 	$scope.repayementCalc = function(loanAmount, interestRate, numberMonths) {
 		monthlyRate = (interestRate/12) / 100
-		var test =  monthlyRate * (Math.pow((1 + monthlyRate), numberMonths))
+		var test =  loanAmount * (monthlyRate * (Math.pow((1+monthlyRate), numberMonths))) / ((Math.pow((1+monthlyRate), numberMonths)) - 1)
 		console.log(test)
 	}
 
